e6c062c96e8b1abf275cc19c856251cca823819b
Nawaz67 <113030826+Nawaz67@users.noreply.github.com>
2022-09-27 17:37:16 +0530

    Add files via upload

---
+++ BookShowImp/BookShow/Program.cs
@@ -0,0 +1,38 @@
+﻿using BookMyShowPresentation;
+using System;
+
+namespace BookShowImp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MoviePL moviepl = new MoviePL();
+            TheatrePL theatrePL = new TheatrePL();  
+            ShowTimingPL showTimingPL=new ShowTimingPL();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("--------------------Welcome to Movie--------------");
+            Console.WriteLine("1) Press 1 to enter Movie Section");
+            Console.WriteLine("2) Press 2 to enter Theatre Section");
+            Console.WriteLine("3) Press 3 to enter Show Time Section");
+            Console.WriteLine("4) Press 4 to exit");
+            int sw=Convert.ToInt32(Console.ReadLine());
+            switch (sw)
+            {
+                case 1:
+                    moviepl.MovieSection();
+                    break;
+                case 2:
+                    theatrePL.TheatreSection();
+                    break;
+                case 3:
+                    showTimingPL.ShowTimeSection();
+                    break;
+                case 4:
+                    Console.ReadLine();
+                    break;
+            }
+
+        }
+    }
+}
