11b4a2646e5c720863545d6bf7788bc32c7e1e88
Nawaz67 <113030826+Nawaz67@users.noreply.github.com>
2022-10-07 17:07:16 +0530

    Add files via upload

---
+++ FoodManagement/FoodManagement/Program.cs
@@ -0,0 +1,22 @@
+﻿using FoodManagementPresentation;
+using System;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace FoodManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------ Welcome to Food Court Management ---------");
+            AdminPL adminPL = new AdminPL();
+            adminPL.Login();
+            Console.ReadLine();
+        }
+    }
+}
+
+
