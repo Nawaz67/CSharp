fc6862ddca7e374e900c20bd9a7508d4c47e65db
Nawaz67 <113030826+Nawaz67@users.noreply.github.com>
2022-10-07 16:45:22 +0530

    Add files via upload

---
+++ FoodManagement/FoodManagementEntity/FoodCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FoodManagementEntity
+{
+    public class FoodCategory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        [ForeignKey("FoodItem")]
+        public int Id { get; set; }
+        public FoodItem FoodItem { get; set; }
+        
+    }
+}
---
+++ FoodManagement/FoodManagementEntity/FoodItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodManagementEntity
+{
+    public class FoodItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FoodName { get; set; }
+        public double FoodPrice { get; set; }
+
+    }
+}
